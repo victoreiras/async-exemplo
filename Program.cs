710f74e1ec1e3b3b3be3173fa09bfb5d2c90c732
Victor Eiras <victor.eiras@gmail.com>
2024-01-31 08:21:29 -0300

    Refatoração

--- Program.cs
+++ Program.cs
@@ -4,27 +4,11 @@ Console.ReadKey();
 
 Console.WriteLine("Inicio");
 
-// Tarefa1();
-// Tarefa2();
 await Tarefa1Async();
 Tarefa2Async();
 
 Console.WriteLine("Fim");
 Console.ReadLine();
-
-// void Tarefa1()
-// {
-//     Console.WriteLine("Iniciando tarefa 1");
-//     Thread.Sleep(6000);
-//     Console.WriteLine("Fim da tarefa 1");
-// }
-
-// void Tarefa2()
-{
-    Console.WriteLine("Iniciando tarefa 1");
-    Thread.Sleep(3000);
-    Console.WriteLine("Fim da tarefa 1");
-}
 
 async Task Tarefa1Async()
 {
